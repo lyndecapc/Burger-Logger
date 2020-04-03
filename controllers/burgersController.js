13d08a96e5aae8db850a857fe6ba61692bc7f70f
Lynde Capogreco <lyndecapc@gmail.com>
2020-04-02 20:30:51 -0500

    get and post routes

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -5,17 +5,33 @@ var burger = require("../models/burger.js");
 
 // get route -> index
 router.get("/", function(req, res) {
-  res.redirect("/burgers");
-});
-
-router.get("/burgers", function(req, res) {
-  //FIXME:
+  burger.selectAll(function(data) {
+    var hndbrsObj = {
+      burgers:data
+    };
+    console.log(hndbrsObj);
+    res.render("index" , hndbrsObj);
+  })
 });
 
 // post route -> back to index
 
+router.post("/api/burgers", function(req, res) {
+  burger.insertOne(
+    ["burger_name", "devoured"],
+    [req.body.burger_name, req.body.devoured],
+    function(result) {
+      res.json({ id:result.insertId });
+    }
+  );
+});
+
+
 
 // put route -> back to index
+
+
+//delete route
 
 
 module.exports = router;
