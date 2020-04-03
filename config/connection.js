5245b248ef11c5b912968470af937dc32afc0eb8
Lynde Capogreco <lyndecapc@gmail.com>
2020-04-02 20:10:18 -0500

    updated dotenv file

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  user: "lynde",
-  password: "capogrlj54",
+  user: process.env.DBUSER,
+  password: process.env.DBPASS,
   database: "burgers_db"
 });
 
