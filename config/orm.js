774d42b3040ef672fd870fda74ec862804074201
Lynde Capogreco <lyndecapc@gmail.com>
2020-04-05 18:37:48 -0500

    fixed html index page so entered burger displays on page

--- config/orm.js
+++ config/orm.js
@@ -4,23 +4,23 @@
 var connection = require("./connection.js");
 
 var orm = {
-  selectAll: function(tableName, cbModel){
-    connection.query("select * from ?? ", tableName, function(err,data){
+  selectAll: function (tableName, cbModel) {
+    connection.query("select * from ?? ", tableName, function (err, data) {
       cbModel(data)
     })
   },
-  insertOne: function(tableName, columnNames, values, cbModel){
-var sqlStatement =     connection.query("insert into ?? (??, ??) values(?, ?)", [tableName, columnNames[0], columnNames[1], values[0], values[1]], function(err,data){
+  insertOne: function (tableName, columnNames, values, cbModel) {
+    var sqlStatement = connection.query("insert into ?? (??, ??) values(?, ?)", [tableName, columnNames[0], columnNames[1], values[0], values[1]], function (err, data) {
       cbModel(data)
     })
     console.log(sqlStatement.sql)
   },
-   updateOne: function(tableName, columnNames, values, cbModel){
-   var sqlStatement =   connection.query("update ?? set ?? = ? where ?? = ?", [tableName, columnNames[0], values[0], columnNames[1], values[1]], function(error, data){
-       cbModel(data)
-     })
+  updateOne: function (tableName, columnNames, values, cbModel) {
+    var sqlStatement = connection.query("update ?? set ?? = ? where ?? = ?", [tableName, columnNames[0], values[0], columnNames[1], values[1]], function (error, data) {
+      cbModel(data)
+    })
 
-     console.log(sqlStatement.sql)
+    console.log(sqlStatement.sql)
   }
 }
 
